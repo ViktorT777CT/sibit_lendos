63e0248ecce379da4fbcbfd8ef2d181b8ad44233
@v.prasolov <p_viktor91@mail.ru>
2023-09-10 19:28:07 +0600

    промежуточное

---
+++ js/js.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        items:2,
+        loop:false,
+        center:true,
+        autoWidth: true,
+        margin:20
+    });
+
+});
